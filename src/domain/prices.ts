56d8fd1b40a74578b051d03469e9bf8153801549
uniperp <uniperp@github.com>
2023-04-13 09:36:27 +0800

    candleprices calc from when there is limit params

--- src/domain/prices.ts
+++ src/domain/prices.ts
@@ -75,7 +75,10 @@ export async function getLimitChartPricesFromStats(chainId, symbol, period, limi
     symbol = getNativeToken(chainId).symbol;
   }
 
-  const url = `${getServerUrlNew(chainId, '/candleprices')}?symbol=${symbol}&period=${period}&source=fast&from=0&chainId=${chainId}&limit=${limit}`;
+  const timeDiff = CHART_PERIODS[period] * (limit + 1);
+  const from = Math.floor(Date.now() / 1000 - timeDiff);
+
+  const url = `${getServerUrlNew(chainId, '/candleprices')}?symbol=${symbol}&period=${period}&source=fast&from=${from}&chainId=${chainId}&limit=${limit}`;
 
   try {
     const response = await fetch(url);
